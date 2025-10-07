622c0b95f2b68c3c55f950143288549063090940
Jared Wasinger <j-wasinger@hotmail.com>
2025-10-07 18:23:24 +0800

    encode BALs as hex strings

--- core/types/bal/bal_encoding_json.go
+++ core/types/bal/bal_encoding_json.go
@@ -3,6 +3,8 @@ package bal
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
 func (c *ContractCode) MarshalJSON() ([]byte, error) {
@@ -74,4 +76,32 @@ func (e *encodingAccountNonce) UnmarshalJSON(data []byte) error {
 		}
 	}
 	return nil
+}
+
+// UnmarshalJSON implements json.Unmarshaler to decode from RLP hex bytes
+func (b *BlockAccessList) UnmarshalJSON(input []byte) error {
+	// Handle both hex string and object formats
+	var hexBytes hexutil.Bytes
+	if err := json.Unmarshal(input, &hexBytes); err == nil {
+		// It's a hex string, decode from RLP
+		return rlp.DecodeBytes(hexBytes, b)
+	}
+
+	// Otherwise try to unmarshal as structured JSON
+	var tmp []AccountAccess
+	if err := json.Unmarshal(input, &tmp); err != nil {
+		return err
+	}
+	*b = BlockAccessList(tmp)
+	return nil
+}
+
+// MarshalJSON implements json.Marshaler to encode as RLP hex bytes
+func (b BlockAccessList) MarshalJSON() ([]byte, error) {
+	// Encode to RLP then to hex
+	rlpBytes, err := rlp.EncodeToBytes(b)
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(hexutil.Bytes(rlpBytes))
 }
